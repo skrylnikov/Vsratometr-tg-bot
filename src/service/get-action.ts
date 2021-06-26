66f14ebb332d4d0f52f81d2983c307783e137289
Dmitriy Skrylnikov <dskrylnikov@vigo.ru>
2021-06-26 16:55:24 +0300

    update random revert

--- src/service/get-action.ts
+++ src/service/get-action.ts
@@ -8,24 +8,33 @@ export enum Action {
   plusAndMinus,
 }
 
-const randomRevertAction = () => {
+const randomRevertAction = (rate?: number) => {
+  if(!rate){
+    return false;
+  }
   const random = Math.random();
-  return random <= 0.01;
+  return random <= (0.01 * rate);
 };
 
 export const getAction = (text: string): Action => {
   const token = getFirstToken(text);
   const result = token.map((token) => {
-    switch (true) {
-      case plusAndMinusList.some((x) => token === x):
-        return Action.plusAndMinus;
-      case plusList.some((x) => token === x):
-        return randomRevertAction() ? Action.minus : Action.plus;
-      case minusList.some((x) => token === x):
-        return randomRevertAction() ? Action.plus : Action.minus;
-      default:
-        return Action.none;
+    const plus = plusList.find((x) => token === x.token);
+    const minus = minusList.find((x) => token === x.token);
+
+    if(plusAndMinusList.some((x) => token === x.token)){
+      return Action.plusAndMinus;
     }
+
+    if(plus){
+      return randomRevertAction(plus.randomRevertRate) ? Action.minus : Action.plus;
+    }
+
+    if(minus){
+      return randomRevertAction(minus.randomRevertRate) ? Action.plus : Action.minus;
+    }
+
+    return Action.none;
   });
 
   return result.isNone() ? Action.none : result.value;
