3ad645573891e6cad520cff3b7a7b1bedcacdf07
Dmitriy Skrylnikov <dskrylnikov@vigo.ru>
2021-06-27 19:18:33 +0300

    up

--- src/controllers/reply-stats.ts
+++ src/controllers/reply-stats.ts
@@ -13,7 +13,7 @@ const convertLevel = (value: number) => {
     case 1: return '🥇';
     case 2: return '🥈';
     case 3: return '🥉';
-    default: return '  ' + value + '.';
+    default: return '   ';
   }
 }
 
@@ -48,10 +48,10 @@ export const replyStats = async (ctx: Context) => {
       const userLink = `[${user?.name || 'Анонимус'}](tg://user?id=${userId})`
 
       const date = created || new Date();
-      const time = `${getDay(new Date()) !== getDay(date) ? 'Вчера ' : ''}в ${format(date, 'H:m')}`;
+      const time = `${getDay(new Date()) !== getDay(date) ? 'вчера ' : ''}в ${format(date, 'H:m')}`;
 
 
-      return `${convertLevel(i + 1)} ${userLink} ${time} [отправил сообщение](${url}) и получил ${value} реплаев`
+      return `${convertLevel(i + 1)} ${userLink} ${time} :${value} реплаев, [говно](${url})`
       // return `${convertLevel(i + 1)} score: ${value}, ${userLink} [сообщение ${format(created || new Date(), 'd MMMM H:m', { locale })}](${url})`
     }),
     (x) => x.join('\n'),
