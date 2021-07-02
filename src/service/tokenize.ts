ca7262992ec00d71621859183036d7cc2e188c17
Dmitriy Skrylnikov <dskrylnikov@vigo.ru>
2021-07-02 10:55:40 +0300

    fix

--- src/service/tokenize.ts
+++ src/service/tokenize.ts
@@ -1,13 +1,14 @@
 import { just, Maybe, none } from '@sweet-monads/maybe';
 
 const tokenize = (text: string) => {
-  return text.toLowerCase().split(/\s|\.|,|!|\?/).filter((x) => x.length !== 0);
+  // return text.toLowerCase().split(/\s|\.|,|!|\?/).filter((x) => x.length !== 0);
+  return text.toLowerCase().split(/|\.|,|!|\?|\(|\)/).filter((x) => x.length !== 0);
 }
 
 export const getFirstToken = (text: string): Maybe<string> => {
   const tokenList = tokenize(text);
 
-  if (tokenList.length === 0 || tokenList.length > 1) {
+  if (tokenList.length === 0) {
     return none();
   }
 
